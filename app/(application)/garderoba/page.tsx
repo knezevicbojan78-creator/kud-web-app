0948ab967a8eb9e6a1a4c24779376b362c33a3b4
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:12:31 +0200

    Create page.tsx

---
+++ app/(application)/garderoba/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function GarderobaPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Garderoba"
+      title="Garderoba"
+      description="Ovde će biti pregled nošnji, opreme i zaduženja garderobe."
+    />
+  );
+}
