8a0370d264d76f9247f526f383045a30a188099a
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:09:36 +0200

    Create page.tsx

---
+++ app/(application)/moje-sekcije/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function MojeSekcijePage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Moje sekcije"
+      title="Moje sekcije"
+      description="Ovde će biti prikazan pregled sekcija koje su povezane sa korisnikom."
+    />
+  );
+}
