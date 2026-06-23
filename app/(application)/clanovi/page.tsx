8e09321c1a2a9854a67958307daeeeffa61681f4
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:10:54 +0200

    Create page.tsx

---
+++ app/(application)/clanovi/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function ClanoviPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Članovi"
+      title="Članovi"
+      description="Ovde će biti lista članova društva i osnovni podaci o njima."
+    />
+  );
+}
