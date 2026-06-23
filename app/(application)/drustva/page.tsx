78194e0f8ee7a8553ab614d66d83eb7870404c61
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:48:17 +0200

    Create page.tsx

---
+++ app/(application)/drustva/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function DrustvaPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Društva"
+      title="Društva"
+      description="Ovde će Master admin moći da vidi i upravlja registrovanim društvima."
+    />
+  );
+}
