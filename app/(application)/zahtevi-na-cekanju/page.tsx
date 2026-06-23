4c3a3ecfadc69a5107869bff4a1bfe1a002adddd
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:50:17 +0200

    Create page.tsx

---
+++ app/(application)/zahtevi-na-cekanju/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function ZahteviNaCekanjuPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Zahtevi na čekanju"
+      title="Zahtevi na čekanju"
+      description="Ovde će biti prikazani zahtevi društava koji čekaju pregled i odobrenje."
+    />
+  );
+}
