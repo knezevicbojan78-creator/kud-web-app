b0024b6d6c93cb1ffa10db42da097b512ca0de0d
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:13:31 +0200

    Create page.tsx

---
+++ app/(application)/koncerti/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function KoncertiPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Koncerti"
+      title="Koncerti"
+      description="Ovde će biti planiranje koncerata, nastupa i drugih događaja."
+    />
+  );
+}
