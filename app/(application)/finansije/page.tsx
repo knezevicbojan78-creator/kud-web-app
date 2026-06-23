6e08a0d78f69d4a0e24e0393d16f35affcc9bbb6
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:11:23 +0200

    Create page.tsx

---
+++ app/(application)/finansije/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function FinansijePage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Finansije"
+      title="Finansije"
+      description="Ovde će biti pregled članarina, uplata, dugovanja i osnovnih finansijskih podataka."
+    />
+  );
+}
