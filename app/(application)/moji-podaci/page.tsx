7dd9ce561eb34b8454042e93404000667f8238ae
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:14:26 +0200

    Create page.tsx

---
+++ app/(application)/moji-podaci/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function MojiPodaciPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Moji podaci"
+      title="Moji podaci"
+      description="Ovde će korisnik moći da vidi i uredi svoje osnovne podatke."
+    />
+  );
+}
