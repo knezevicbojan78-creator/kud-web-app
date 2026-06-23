c95b5a7e6aea76a233b595975502bbcaf7b257f6
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:51:25 +0200

    Create page.tsx

---
+++ app/(application)/moja-deca/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function MojaDecaPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Moja deca"
+      title="Moja deca"
+      description="Ovde će roditelj videti samo svoju decu i njihove osnovne informacije."
+    />
+  );
+}
