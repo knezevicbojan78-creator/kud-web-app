10cc530cd94a28ddd3417942407804e7548bdb34
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:10:30 +0200

    Create page.tsx

---
+++ app/(application)/prisustvo/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function PrisustvoPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Prisustvo"
+      title="Prisustvo"
+      description="Ovde će biti evidencija dolazaka članova na probe, nastupe i druge aktivnosti."
+    />
+  );
+}
