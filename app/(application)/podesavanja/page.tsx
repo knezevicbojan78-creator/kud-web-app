9a1e3122902ac136c7100391380907f2b50dedec
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:14:00 +0200

    Create page.tsx

---
+++ app/(application)/podesavanja/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function PodesavanjaPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Podešavanja"
+      title="Podešavanja"
+      description="Ovde će biti osnovna podešavanja aplikacije i društva."
+    />
+  );
+}
