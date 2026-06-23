aacd94392ae00a40332b00e8c8e253dea0d979ad
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:13:02 +0200

    Create page.tsx

---
+++ app/(application)/izvestaji/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function IzvestajiPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Izveštaji"
+      title="Izveštaji"
+      description="Ovde će biti priprema i pregled izveštaja za rad društva."
+    />
+  );
+}
