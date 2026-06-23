212c24571ec7c3f9bf7f9c19e4f6eb514f6ef134
knezevicbojan78-creator <knezevic.bojan78@gmail.com>
2026-06-23 09:50:56 +0200

    Create page.tsx

---
+++ app/(application)/podesavanja-sistema/page.tsx
@@ -0,0 +1,11 @@
+import { PlaceholderPage } from "../../_components/PlaceholderPage";
+
+export default function PodesavanjaSistemaPage() {
+  return (
+    <PlaceholderPage
+      eyebrow="Podešavanja sistema"
+      title="Podešavanja sistema"
+      description="Ovde će biti globalna podešavanja sistema dostupna samo Master admin ulozi."
+    />
+  );
+}
